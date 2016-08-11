c8e485cc5a8020d1fbfb6eb26244afb4e6950d28
Ben Sullivan <benskill@gmail.com>
2016-08-11 10:32:25 +0100

    Initial Commit

---
+++ CountryTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CountryTrainer
+//
+//  Created by Ben Sullivan on 11/08/2016.
+//  Copyright © 2016 Ben Sullivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
